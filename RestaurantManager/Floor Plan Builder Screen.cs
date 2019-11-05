e31a1309ac2e1f89951f07d638d8da2e1ce66bab
Eamonn Alphin <eamonnalphin@gmail.com>
2019-11-04 21:31:05 -0800

    added db? maybe?

---
+++ RestaurantManager/Floor Plan Builder Screen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestaurantManager
+{
+    public partial class Floor_Plan_Builder_Screen : Form
+    {
+        public Floor_Plan_Builder_Screen()
+        {
+            InitializeComponent();
+        }
+    }
+}
